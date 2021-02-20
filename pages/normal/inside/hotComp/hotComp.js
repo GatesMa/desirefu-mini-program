79d516e2f4e99e8f3f9ace03504ecc5c1bc0c557
gatesma <gatesma@foxmail.com>
2021-02-20 21:35:13 +0800

    联调1

--- pages/normal/inside/hotComp/hotComp.js
+++ pages/normal/inside/hotComp/hotComp.js
@@ -15,6 +15,64 @@ Page({
 
   },
 
+  // 获取比赛列表
+  fillRank() {
+    this.setData({
+      loadModal: true
+    })
+
+    this.getCompetitionData().then((list) => {
+      // console.log('competitionList:', list)
+      this.setData({
+        competitions: list,
+        loadModal: false
+      })
+    })
+  },
+
+  getCompetitionData() {
+    return new Promise((resolve, reject) => {
+
+      wx.request({
+        url: app.globalData.baseUrl + '/desire_fu/v1/competition/select_scroll',
+        data: {
+          status: 1, // 默认只获取正常状态，不获取草稿
+          state: this.data.state == -1 ? null : this.data.state,
+          type: this.data.type == -1 ? null : this.data.type,
+          sort_field: this.data.sortField,
+          sort_seq: this.data.sortType,
+          title: this.data.title ? this.data.title : null,
+          founder: this.data.founder ? this.data.founder : null,
+          page: {
+            page_num: this.data.page,
+            page_size: this.data.pageSize
+          }
+        },
+        method: "POST",
+        header: {
+          'content-type': 'application/json',
+          'Accept': 'application/json'
+        },
+        success: (res) => {
+          resolve(res.data.data.list);
+        },
+        fail: (err) => {
+          wx.showToast({
+            title: '获取数据异常',
+            icon: 'none',
+            duration: 2000
+          })
+          this.setData({
+            loadModal: false
+          })
+          reject(err);
+        }
+      })
+    })
+
+  },
+
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
