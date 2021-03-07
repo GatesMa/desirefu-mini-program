9a779ce6f084ca433ac37c51fe294bb16c8bc9a1
gatesma <gatesma@foxmail.com>
2021-03-07 15:59:24 +0800

    添加item

--- pages/oss/component/homepage/homepage.js
+++ pages/oss/component/homepage/homepage.js
@@ -17,117 +17,23 @@ Component({
 
   /* 组件的初始数据 */
   data: {
-    cardCur: 0,
-    swiperList: [],
-    loadModal: false, // 是否加载
-
-    // 列表相关
-    competitions: [], // 比赛列表
-    page: 1,
-    pageSize: 5,
-
-    // model相关
-    modalName: null,
-    // 状态选择 
-    state: -1,
-    stateName: '全部',
-    stateList: [{
-      state: '-1',
-      stateName: '全部'
-    }, {
-      state: '0',
-      stateName: '未开始'
-    }, {
-      state: '1',
-      stateName: '进行中'
-    }, {
-      state: '2',
-      stateName: '已结束'
-    }],
-    // 状态选择 
-    type: -1,
-    typeName: '全部',
-    typeList: [{
-      type: '-1',
-      typeName: '全部'
-    }, {
-      type: '0',
-      typeName: '院级'
-    }, {
-      type: '1',
-      typeName: '校级'
-    }, {
-      type: '2',
-      typeName: '省级'
-    }, {
-      type: '3',
-      typeName: '国家级'
-    }, {
-      type: '4',
-      typeName: '国际级'
-    }],
-    // 省级选择 
-    type: -1,
-    typeName: '全部',
-    typeList: [{
-      type: '-1',
-      typeName: '全部'
-    }, {
-      type: '0',
-      typeName: '院级'
-    }, {
-      type: '1',
-      typeName: '校级'
-    }, {
-      type: '2',
-      typeName: '省级'
-    }, {
-      type: '3',
-      typeName: '国家级'
-    }, {
-      type: '4',
-      typeName: '国际级'
-    }],
-    // 排序选择 
-    sortField: 'pv',
-    sortType: 'desc',
-    sortList: [{
-      sortField: 'pv',
-      sortType: 'desc',
-      name: '浏览量高-低'
-    }, {
-      sortField: 'pv',
-      sortType: 'asc',
-      name: '浏览量低-高'
-    }, {
-      sortField: 'createdTime',
-      sortType: 'desc',
-      name: '创建时间晚-早'
-    }, {
-      sortField: 'createdTime',
-      sortType: 'asc',
-      name: '创建时间早-晚'
-    }, {
-      sortField: 'beginTime',
-      sortType: 'desc',
-      name: '开始时间晚-早'
-    }, {
-      sortField: 'beginTime',
-      sortType: 'asc',
-      name: '开始时间早-晚'
-    }, {
-      sortField: 'endTime',
-      sortType: 'desc',
-      name: '结束时间晚-早'
-    }, {
-      sortField: 'endTime',
-      sortType: 'asc',
-      name: '结束时间早-晚'
-    }],
-    // 标题
-    title: null,
-    // founder
-    founder: null
+    gridCol: 5,
+    list: [{
+        title: '首页轮播图',
+        img: 'https://dfu-1257282228.cos.ap-chengdu.myqcloud.com/dfu/oss-1.jpg',
+        url: '/idx-swiper/idx-swiper'
+      },
+      {
+        title: '广场公告',
+        img: 'https://dfu-1257282228.cos.ap-chengdu.myqcloud.com/dfu/oss-2.jpg',
+        url: '/park-notice/park-notice'
+      },
+      {
+        title: '广场轮播图',
+        img: 'https://dfu-1257282228.cos.ap-chengdu.myqcloud.com/dfu/oss-3.jpg',
+        url: '/park-swiper/park-swiper'
+      }
+    ]
   },
 
   /* 组件声明周期函数 */
@@ -136,8 +42,7 @@ Component({
 
     },
     attached: function () {
-      this.fillCompetitions()
-      this.getNotification() // 通告
+
     },
     moved: function () {
 
@@ -149,228 +54,11 @@ Component({
 
   /* 组件的方法列表 */
   methods: {
-    // cardSwiper
-    cardSwiper(e) {
-      this.setData({
-        cardCur: e.detail.current
-      })
-    },
-    showModal(e) {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    },
-    hideModal(e) {
-      this.setData({
-        modalName: null
-      })
-    },
-    chooseState(e) {
-      var item = e.currentTarget.dataset.item
-      // 修改数据同时隐藏model
-      this.setData({
-        state: item.state,
-        stateName: item.stateName,
-        modalName: null
-      })
-    },
-    chooseType(e) {
-      var item = e.currentTarget.dataset.item
-      // 修改数据同时隐藏model
-      this.setData({
-        type: item.type,
-        typeName: item.typeName,
-        modalName: null
-      })
-    },
-    chooseSort(e) {
-      var item = e.currentTarget.dataset.item
-      // 修改数据同时隐藏model
-      this.setData({
-        sortField: item.sortField,
-        sortType: item.sortType,
-        modalName: null
-      })
-      // 提升修改
-      wx.showToast({
-        title: '修改排序方式为：' + item.name,
-        icon: 'none',
-        duration: 1000
-      })
-    },
-    // 标题input
-    titleinput(e) {
-      this.setData({
-        title: e.detail.value
-      })
-    },
-    // founderinput
-    founderinput(e) {
-      this.setData({
-        founder: e.detail.value
-      })
-    },
-    scrollBack() {
-      // 第一页不允许翻页
-      if (this.data.page == 1) {
-        wx.showToast({
-          title: '已是第一页',
-          icon: 'none',
-          duration: 2000
-        })
-        return
-      }
-
-      this.setData({
-        page: this.data.page - 1,
-        loadModal: true
-      })
-
-      this.getCompetitionData().then((list) => {
-        // console.log('competitionList:', list)
-        this.setData({
-          competitions: list,
-          loadModal: false
-        })
-      })
-    },
-    scrollFront() {
-
-      this.setData({
-        page: this.data.page + 1,
-        loadModal: true
-      })
-
-      this.getCompetitionData().then((list) => {
-        // console.log('competitionList:', list)
-        // console.log('list.length, ', list.length)
-        // console.log('list, ', list == null)
-        // 这一页是空的
-        if (list == null || list.length == 0) {
-          wx.showToast({
-            title: '已是最后一页',
-            icon: 'none',
-            duration: 2000
-          })
-          // page设置回去
-          this.setData({
-            page: this.data.page - 1,
-            loadModal: false
-          })
-        } else {
-          this.setData({
-            competitions: list,
-            loadModal: false
-          })
-        }
-      })
-    },
-    // 获取比赛列表
-    fillCompetitions() {
-      this.setData({
-        loadModal: true
-      })
-
-      this.getCompetitionData().then((list) => {
-        // console.log('competitionList:', list)
-        this.setData({
-          competitions: list,
-          loadModal: false
-        })
-      })
-    },
-
-    getCompetitionData() {
-      return new Promise((resolve, reject) => {
-
-        wx.request({
-          url: app.globalData.baseUrl + '/desire_fu/v1/competition/select_scroll',
-          data: {
-            status: 1, // 默认只获取正常状态，不获取草稿
-            state: this.data.state == -1 ? null : this.data.state,
-            type: this.data.type == -1 ? null : this.data.type,
-            sort_field: this.data.sortField,
-            sort_seq: this.data.sortType,
-            title: this.data.title ? this.data.title : null,
-            founder: this.data.founder ? this.data.founder : null,
-            page: {
-              page_num: this.data.page,
-              page_size: this.data.pageSize
-            }
-          },
-          method: "POST",
-          header: {
-            'content-type': 'application/json',
-            'Accept': 'application/json'
-          },
-          success: (res) => {
-            // console.log('res:', res.data)
-            // console.log('canLoginAccountData:' + canLoginAccountData)
-            // this.setData({
-            //   competitions: res.data.data.list
-            // })
-            resolve(res.data.data.list);
-          },
-          fail: (err) => {
-            wx.showToast({
-              title: '获取数据异常',
-              icon: 'none',
-              duration: 2000
-            })
-            this.setData({
-              loadModal: false
-            })
-            reject(err);
-          }
-        })
-      })
-
-    },
-
-    // 公告栏
-    getNotification() {
-      wx.request({
-        url: app.globalData.baseUrl + '/desire_fu/v1/notification/select',
-        data: {
-          "status": 1,
-          "type": 1
-        },
-        method: "POST",
-        header: {
-          'content-type': 'application/json',
-          'Accept': 'application/json'
-        },
-        success: (res) => {
-          // console.log('res:', res.data.data.list)
-          this.setData({
-            swiperList: res.data.data.list
-          })
-        },
-        fail: (err) => {
-          wx.showToast({
-            title: '获取数据异常,可退出重试',
-            icon: 'none',
-            duration: 2000
-          })
-        }
-      })
-    },
-
-    navigateToNotice: function (e) {
-      var item = e.currentTarget.dataset.item
-      var competitionId = item.content // 比赛的id放在content里了
+    toChild(e) {
       wx.navigateTo({
-        url: '/pages/normal/preview/preview?isPre=false&hasId=true&competitionId=' + competitionId
+        url: '/pages/oss/inside' + e.currentTarget.dataset.url
       })
     },
-
-    // 跳转到详情页面
-    navToPreview(e) {
-      wx.navigateTo({
-        url: '/pages/normal/preview/preview?isPre=false&showFoldBtn=true&competition=' + encodeURIComponent(JSON.stringify(e.currentTarget.dataset.item))
-      })
-    }
-
   }
 
 })
---
+++ pages/oss/inside/idx-swiper/idx-swiper.js
@@ -0,0 +1,220 @@
+// pages/oss/inside/idx-swiper/idx-swiper.js
+var app = getApp();
+var common = require("../../../../common.js")
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperList: [],
+
+    imgList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getSwiperList()
+  },
+
+  // 公告栏
+  getSwiperList() {
+    wx.request({
+      url: app.globalData.baseUrl + '/desire_fu/v1/notification/select',
+      data: {
+        "status": 1,
+        "type": 1
+      },
+      method: "POST",
+      header: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+      },
+      success: (res) => {
+        console.log('res:', res.data.data.list)
+        this.setData({
+          swiperList: res.data.data.list
+        })
+      },
+      fail: (err) => {
+        wx.showToast({
+          title: '获取数据异常,可退出重试',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  },
+
+  navigateToNotice: function (e) {
+    var item = e.currentTarget.dataset.item
+    var competitionId = item.content // 比赛的id放在content里了
+    wx.navigateTo({
+      url: '/pages/competition/preview/preview?isPre=false&hasId=true&competitionId=' + competitionId
+    })
+  },
+
+  // 图片选择框
+  ChooseImage() {
+    wx.chooseImage({
+      count: 4, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            imgList: this.data.imgList.concat(res.tempFilePaths)
+          })
+        } else {
+          this.setData({
+            imgList: res.tempFilePaths
+          })
+        }
+      }
+    });
+  },
+  ViewImage(e) {
+    wx.previewImage({
+      urls: this.data.imgList,
+      current: e.currentTarget.dataset.url
+    });
+  },
+  DelImg(e) {
+    wx.showModal({
+      title: '你好',
+      content: '确定要删除这张图片吗？',
+      cancelText: '再看看',
+      confirmText: '再见',
+      success: res => {
+        if (res.confirm) {
+          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+          this.setData({
+            imgList: this.data.imgList
+          })
+        }
+      }
+    })
+  },
+
+  // id
+  idInput(e) {
+    this.setData({
+      comId: e.detail.value
+    })
+  },
+
+  addItem() {
+    console.log(this.data.imgList)
+    console.log(this.data.comId)
+    if (this.data.imgList.length == 0) {
+      wx.showToast({
+        title: '请上传图片',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    if (this.data.comId == null || this.data.comId == '') {
+      wx.showToast({
+        title: '请输入要跳转的比赛ID',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    common.uploadFile(this.data.imgList[0], 'IMG').then((url) => {
+      this.requestCreateNotice(url, this.data.comId)
+    }).catch(()=> {
+      wx.showToast({
+        title: '出现问题',
+        icon: 'none',
+        duration: 1000
+      })
+    })
+  },
+
+  requestCreateNotice(front_img, content) {
+    var that = this
+    wx.request({
+      url: app.globalData.baseUrl + '/desire_fu/v1/notification/add',
+      method: 'POST',
+      data: {
+        type: 1,
+        front_img: front_img,
+        content: content
+      },
+      header: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+      },
+      success(res) {
+        console.log(res.data)
+        wx.showToast({
+          title: '请求成功',
+          icon: 'success',
+          duration: 1000
+        })
+        var tmp = [res.data.data]
+        console.log(tmp)
+        that.setData({
+          swiperList: that.data.swiperList.concat(tmp)
+        })
+      }
+    })
+  },
+
+  deleteItem(e) {
+    console.log(e.currentTarget.dataset.item)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
