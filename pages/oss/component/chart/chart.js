7f93412a68342d9a6859eaba55c066b1116e3a05
gatesma <gatesma@foxmail.com>
2021-03-07 22:53:50 +0800

    比赛热度可视化

--- pages/oss/component/chart/chart.js
+++ pages/oss/component/chart/chart.js
@@ -10,8 +10,9 @@ Component({
   /* 组件的初始数据 */
   data: {
     elements: [{
-        title: '布局',
-        name: 'layout',
+        title: '比赛热度',
+        name: 'Hot Rank',
+        url: 'bar',
         color: 'cyan',
         icon: 'newsfill'
       },
@@ -62,87 +63,7 @@ Component({
 
   /* 组件的方法列表 */
   methods: {
-    // 关闭通知
-    closeNotice(e) {
-      var that = this;
-      console.log(e);
-      that.setData({
-        hideNoticeAni: true
-      })
-      // 500ms后隐藏元素
-      setTimeout(function () {
-        that.setData({
-          hideNotice: true
-        })
-      }, 500)
-    },
-    showNotice(e) {
-      this.setData({
-        showModel: true
-      })
-    },
-    hideModal(e) {
-      this.setData({
-        showModel: false
-      })
-    },
-    getNotificationType2() {
-      wx.request({
-        url: app.globalData.baseUrl + '/desire_fu/v1/notification/select',
-        data: {
-          "status": 1,
-          "type": 2
-        },
-        method: "POST",
-        header: {
-          'content-type': 'application/json',
-          'Accept': 'application/json'
-        },
-        success: (res) => {
-          console.log('res:', res.data.data.list)
-          if (res.data.data.list && res.data.data.list.lenght != 0) {
-            this.setData({
-              noticeContent: res.data.data.list[0].content
-            })
-          }
-        },
-        fail: (err) => {
-          wx.showToast({
-            title: '获取数据异常,可退出重试',
-            icon: 'none',
-            duration: 2000
-          })
-        }
-      })
-    },
-    // 跳转到通知页
-    navToApplication() {
-      wx.navigateTo({
-        url: '/pages/normal/inside/application/application'
-      })
-    },
-    // 跳转到消息
-    navToMsg() {
-      wx.navigateTo({
-        url: '/pages/normal/inside/msg/msg'
-      })
-    },
-    // 跳转到热门
-    navToHot() {
-      wx.navigateTo({
-        url: '/pages/normal/inside/hotComp/hotComp'
-      })
-    },
-    navToQuestion() {
-      wx.navigateTo({
-        url: '/pages/normal/inside/question/question'
-      })
-    },
-    navToCollect() {
-      wx.navigateTo({
-        url: '/pages/normal/inside/collect/collect'
-      })
-    }
+    
   }
 
 })
